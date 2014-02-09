62f357ab1d4421843f9364b101d0e754b112ebe7
Aaron Smith <asmith1810@gmail.com>
2014-02-09 00:53:33 -0600

    Removed commands to clean from project.sh file because it does not apply to all projects

--- project1.sh
+++ project1.sh
@@ -5,7 +5,3 @@ sudo apt-get install make -y
 sudo apt-get install valgrind -y
 sudo apt-get install libcurl4-gnutls-dev -y
 sudo apt-get install build-essential g++ -y
-cd ..
-cd ..
-cd vagrant
-make clean
