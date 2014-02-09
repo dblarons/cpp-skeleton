2f2442393484794cebe8114f7b0e514038d24639
Aaron Smith <asmith1810@gmail.com>
2014-02-09 01:39:29 -0600

    Updated run.sh to include optional valgrind command

--- run.sh
+++ run.sh
@@ -1,9 +1,29 @@
+# run program:
+# $ bash run.sh
+
+# run program with valgrind:
+# $ bash run.sh valgrind
+
 cd src
+
+echo
+
 make clean
 make
 
-echo
+echo; echo
 echo ---------------
 
-./output
+if [ "$1" == "valgrind" ];
+then
+  valgrind ./output
+else
+  ./output
+fi
+
+echo ---------------
+
+echo; echo
+make clean
+rm output
 cd ..
